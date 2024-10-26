ee39f02eb275e3f1e24d4f55e09b4e953f38e8dd
Himansh Prajapati <165716300+himansh-01@users.noreply.github.com>
2024-10-26 17:28:22 +0530

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,7 +6,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 function Main() {
 
-    const { loginWithRedirect } = useAuth0;
+    const { loginWithRedirect } = useAuth0();
     
     const[toggle, setToggle] = useState(false);
 
