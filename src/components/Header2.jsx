2629dfeba3c7af131d970cf08b72c084a4f5ba34
Himansh Prajapati <165716300+himansh-01@users.noreply.github.com>
2024-10-26 18:26:05 +0530

    Update Header2.jsx

--- src/components/Header2.jsx
+++ src/components/Header2.jsx
@@ -35,9 +35,10 @@ function Header2() {
           <div className='gap-[2rem] flex text-2xl items-center mb-[2rem]'><AiOutlineInfoCircle /> About Us</div>          
           <div className='gap-[2rem] flex text-2xl items-center mb-[2rem]'><FiHelpCircle /> Help and Support</div>          
           <div className='gap-[2rem] flex text-2xl items-center mb-[2rem]'><IoSettingsOutline /> Settings</div>
-          <div className='gap-[2rem] flex text-2xl items-center mb-[1rem]'></div>><MdPowerSettingsNew /> Sign out</div>
+          <div className='gap-[2rem] flex text-2xl items-center mb-[2rem]'><MdPowerSettingsNew /> Sign out</div>          
+
         </div>
-     <div className='w-[100%] h-[100%]'>
+      <div className='w-[100%] h-[100%]'>
         <div className='flex w-[100%] h-[80px] border border-black shadow-lg rounded-xl items-center'>
             <div className='ml-10 font-bold text-xl'>Blogging.com</div>
             <input type='text' placeholder='Search' className='rounded-2xl h-[2rem] ml-[1rem] pl-[0.5rem] border-black border'></input>
@@ -45,12 +46,12 @@ function Header2() {
                 <div className='flex items-center w-[11rem] mr-[6rem]'>
                     <Link to="/post"><div className='flex items-center text-xl cursor-pointer'><FaRegEdit />Post</div></Link>
                     <MdOutlineNotificationsNone className='font-extrabold text-2xl ml-[2rem] mr-[1.5rem] cursor-pointer' />
-                    <div className='w-[2rem] h-[2rem] rounded-full cursor-pointer'><img src={user.picture} alt='https://th.bing.com/th/id/OIP.CG70mC-flvJIYFRVmR9FZwHaHa?rs=1&pid=ImgDetMain' onClick={sideBar}/></div>
+                    <div className='w-[2rem] h-[2rem] rounded-full cursor-pointer'><img src='https://th.bing.com/th/id/OIP.CG70mC-flvJIYFRVmR9FZwHaHa?rs=1&pid=ImgDetMain' alt='' onClick={sideBar}/></div>
                 </div>    
             </div>
         </div>
-     </div>
-   </>
+      </div>
+    </>
   ) 
 }
 
