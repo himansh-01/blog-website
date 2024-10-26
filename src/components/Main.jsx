4831787a089bea94f4afc1c0cb60031c58f68f9a
Himansh Prajapati <165716300+himansh-01@users.noreply.github.com>
2024-10-26 17:17:35 +0530

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { useState } from "react";
 import { RxCross1 } from "react-icons/rx";
 import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Main() {
+
+    const { loginWithRedirect } = useAuth0;
     
     const[toggle, setToggle] = useState(false);
 
   return (
     <div className='w-[100%] h-[89vh] grid'>
-        <div className='top-28 absolute justify-self-center w-[30%] h-[70%] border flex  flex-col space-y-[2rem] items-center rounded-xl bg-[#bdb8b8] duration-500' style={{
+{/*         <div className='top-28 absolute justify-self-center w-[30%] h-[70%] border flex  flex-col space-y-[2rem] items-center rounded-xl bg-[#bdb8b8] duration-500' style={{
                 opacity:toggle ? 1 : 0,
                 visibility:toggle ? 1 : 0
             }}>
@@ -27,12 +30,12 @@ function Main() {
                 <button className='px-2 bg-white rounded-2xl'><Link to="/home">Sign In</Link></button>
               </div>  
             </form>
-        </div>
+        </div> */}
         <div className='w-[100%] border h-[89vh] p-[10rem] flex'>
             <span className='justify-self-center self-center w-[50%] p-9'>
                 <h1 className='text-2xl font-bold'>Blogging isn’t about publishing as much as you can. It’s about publishing as smart as you can</h1>
                 <button className='block mt-[2rem] px-8 py-4 rounded-xl bg-[#cecccc]' onClick={() => {
-                    setToggle(!toggle)
+                     loginWithRedirect()
                 }}>Get Started</button>
             </span>
             <span className='w-[50%] flex justify-center'>
